f5cbd72846886c432cc0dd716c1b79147f459767
Johnny Soong <johnnysoong@hotmail.com>
2018-08-08 00:39:18 +0000

    Done.

--- spec/keys_of_hash_spec.rb
+++ spec/keys_of_hash_spec.rb
@@ -1,3 +1,4 @@
+
 describe 'keys_of' do
 
   let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
